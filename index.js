c6be0e4a956bbfb5b6ac5d883c99437519a5ea00
Dolan Miu <dolan_miu@hotmail.com>
2020-01-11 21:23:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const Nightmare = require("nightmare");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const nightmare = Nightmare({ show: false });
+
+  nightmare
+    .goto("https://www.blackrock.com/tools/pretrade")
+    .wait(5000)
+    .wait("#signin")
+    .type("#userName", req.query.username)
+    .type("#password", req.query.password)
+    .click("#submitLogin")
+    .wait(".application")
+    .cookies.get()
+    .end()
+    .then(cookies => {
+      const cookie = cookies.find(c => c.name === "JSESSION_blk-tools02");
+      console.log(cookie);
+      res.status(200).send(cookie);
+    })
+    .catch(error => {
+      console.error("Cookie Fetch failed", error);
+      res.status(500).send(error);
+    });
+});
+
+app.listen(process.env.PORT || 3001);
